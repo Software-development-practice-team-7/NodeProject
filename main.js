de278d10fc810eac037f915302c4ddfcccf2189b
jaehoon <jh901794@gmail.com>
2021-12-05 00:08:35 +0900

    main page menu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
 
 var express = require("express");
 var app = express();
@@ -10,12 +11,14 @@ var static = require('serve-static');
 var errorHandler = require('error-handler');
 var expressErrorHandler = require('express-error-handler');
 var expressSession = require('express-session');
+var database;
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("views"));
+
 
   //메인 페이지
 app.get("/", function (req, res) {
@@ -28,3 +31,17 @@ app.get("/board.html", function (req, res) {
 });
 
   app.listen(4000, () => console.log("Server is running on port 4000..."));
+
+
+  
+function connectDB(){
+  var databaseUrl = 'mongodb://localhost:27017/local';
+
+  MongoClient.connect(databaseUrl,function(err, db){
+    if(err) throw err;
+
+    console.log('데이터베이스에 연결되었습니다. :' + databaseUrl);
+
+    database = db;
+  });
+}
